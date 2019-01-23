ce42c919314bb196fb22161771d77a063e391ad3
Blaine <blaineburke1999@gmail.com>
2019-01-23 17:28:36 +0000

    Excercise 4

--- Untitled-1.ts
+++ Untitled-1.ts
@@ -1,9 +1,3 @@
-function addition(value:string)
-{
-    console.log("Value is " + value);
-}
-var firstVal =42;
-var secondVal =1;
-var finalVal = (firstVal + secondVal).toLocaleString();
-addition(finalVal);
-
+//Boolean
+let flag: boolean = true;
+console.log("Value assigned to flag is: " + flag);
