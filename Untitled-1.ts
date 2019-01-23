89b78ab40202369b298c459e8dad941ac0c9200d
Blaine <blaineburke1999@gmail.com>
2019-01-23 16:52:33 +0000

    hello

---
+++ Untitled-1.ts
@@ -0,0 +1,9 @@
+function addition(value:string)
+{
+    console.log("Value is " + value);
+}
+var firstVal =42;
+var secondVal =1;
+var finalVal = (firstVal + secondVal).toLocaleString();
+addition(finalVal);
+
